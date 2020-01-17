4e11b364ad8ed14d7b799608c2c81a11910cfaf8
旧梦纷飞 <daohunliwei@vip.qq.com>
2020-01-17 21:58:41 +0800

    先跑起来

---
+++ Src/Loongdream.Metis.WebSite/Controllers/LoginController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Loongdream.Metis.WebSite.Controllers
+{
+    public class LoginController : Controller
+    {
+        /// <summary>
+        /// 常规登陆
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Default()
+        {
+            return View();
+        }
+        /// <summary>
+        /// Ldap登录
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult LDAP()
+        {
+            ViewData["Title"] = "域账户登录";
+            return View();
+        }
+    }
+}
---
+++ Src/Loongdream.Metis.WebSite/Controllers/OtherPageController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Loongdream.Metis.WebSite.Controllers
+{
+    public class OtherPageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 常规登陆
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult I18support()
+        {
+            return View();
+        }
+    }
+}
