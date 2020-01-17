f63bad14e42c17c33560fa4b9ad16aea85905597
旧梦纷飞 <daohunliwei@vip.qq.com>
2020-01-18 03:52:39 +0800

    提测

---
+++ Src/Loongdream.Metis.Infrastructure/Helper/DateTimeHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loongdream.Metis.Infrastructure.Helper
+{
+    /// <summary>
+    /// 时间帮助类
+    /// </summary>
+    public static class DateTimeHelper
+    {
+        /// <summary>
+        /// 时间戳转为C#格式时间
+        /// </summary>
+        /// <param name="timeStamp"></param>
+        /// <returns></returns>
+        public static DateTime StampToDateTime(string timeStamp)
+        {
+            DateTime dateTimeStart = TimeZone.CurrentTimeZone.ToLocalTime(new DateTime(1970, 1, 1));
+            long lTime = long.Parse(timeStamp + "0000000");
+            TimeSpan toNow = new TimeSpan(lTime);
+
+            return dateTimeStart.Add(toNow);
+        }
+
+        /// <summary>
+        /// DateTime时间格式转换为Unix时间戳格式
+        /// </summary>
+        /// <param name="time"></param>
+        /// <returns></returns>
+        public static string GetTimeStamp(this DateTime time)
+        {
+            DateTime startTime = TimeZone.CurrentTimeZone.ToLocalTime(new DateTime(1970, 1, 1));
+            return ((int)(time - startTime).TotalSeconds).ToString();
+        }
+    }
+}
---
+++ Src/Loongdream.Metis.Infrastructure/Helper/JsonHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loongdream.Metis.Infrastructure.Helper
+{
+    public static class JsonHelper
+    {
+        public static string ToJson(this object obj)
+        {
+            return Newtonsoft.Json.JsonConvert.SerializeObject(obj);
+        }
+        public static T ToObj<T>(this string json)
+        {
+            return Newtonsoft.Json.JsonConvert.DeserializeObject<T>(json);
+        }
+    }
+}
